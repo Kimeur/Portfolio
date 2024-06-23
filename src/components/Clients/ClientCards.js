073ab744a13a610cf81e5b1471d4f687c5739a51
Side Hustle <sidehustle@MacBook-Pro.local>
2024-06-24 00:23:56 +0100

    add google analytics

--- src/components/Clients/ClientCards.js
+++ src/components/Clients/ClientCards.js
@@ -7,7 +7,7 @@ import { BsGithub } from "react-icons/bs";
 function ProjectCards(props) {
   return (
     <Card className="project-card-view">
-      <Card.Img variant="top" src={props.imgPath} alt="card-img" style={{height: "100%"}} />
+      <Card.Img variant="top" src={props.imgPath} alt="card-img"  />
       <Card.Body>
         <Card.Title><strong className="purple">{props.title}</strong></Card.Title>
         <Card.Text style={{ textAlign: "justify" }}>
